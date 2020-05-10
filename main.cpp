321952df0bf7269735b2c31321e79569b4b2932e
da <>
2020-05-10 19:55:34 +0200

    add character w/ (very) basic movement

--- main.cpp
+++ main.cpp
@@ -8,11 +8,23 @@
 
 int main()
 {
-    glm::vec3 testvec(1.0f,2.0f,3.0f);
-
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+
+    // TEXTURE (8x9)
+    int xstep = 64;
+    int ystep = 64;
+    sf::Texture texture;
+    if (!texture.loadFromFile("../assets/platformer_sprites_base.png"))
+        return -1;
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.setTextureRect(sf::IntRect(0 * xstep, 8 * ystep, xstep, ystep));
+
+    // MOVEMENT
+    sf::Vector2f rightstep(3.0f, 0);
+    sf::Vector2f leftstep(-3.0f, 0);
 
     // AUDIO
     sf::SoundBuffer sfxBuffer;
@@ -23,28 +35,40 @@ int main()
 
     sf::Music music;
     if (!music.openFromFile("../assets/intro.ogg")) return -1;
+    music.setLoop(true);
     music.play();
 
     // GAMELOOP
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
 
-	    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
+	    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+	        sprite.setTextureRect(sf::IntRect(7 * xstep, 0 * ystep,
+						  -xstep, ystep));
+		sprite.setPosition(sprite.getPosition() + leftstep);
 		sfx.play();
+	    }
+
+	    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+	        sprite.setTextureRect(sf::IntRect(5 * xstep, 7 * ystep,
+						  xstep, ystep));
+		sprite.setPosition(sprite.getPosition() + rightstep);
+		sfx.play();
+	    }
         }
 
         window.clear();
+
+	// DRAW
         window.draw(shape);
+	window.draw(sprite);
+
         window.display();
     }
-
-    printf("%d", helperfunc());
-    printf("%f", testvec[1]);
 
     return 0;
 }
