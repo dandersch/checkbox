d223592b15751b157c0de5c47e14b919fd094202
da <>
2020-05-15 11:01:48 +0200

    add getWorldTransform() to scenenode

---
+++ src/scenenode.cpp
@@ -0,0 +1,54 @@
+#include "scenenode.h"
+
+void SceneNode::attachChild(std::unique_ptr<SceneNode> child)
+{
+    child->m_parent = this;
+    m_children.push_back(std::move(child));
+}
+
+std::unique_ptr<SceneNode> SceneNode::detachChild(const SceneNode& node)
+{
+    auto found = std::find_if(m_children.begin(), m_children.end(),
+                              [&](std::unique_ptr<SceneNode>& p) -> bool {
+                                  return p.get() == &node;
+                              });
+
+    assert(found != m_children.end());
+    std::unique_ptr<SceneNode> result = std::move(*found);
+    result->m_parent = nullptr;
+    m_children.erase(found);
+
+    return result;
+}
+
+void SceneNode::update(float dt)
+{
+    updateCurrent(dt);
+    updateChildren(dt);
+}
+
+void SceneNode::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    states.transform *= getTransform();
+
+    drawCurrent(target, states);
+
+    // draw children
+    for (const auto& child : m_children) child->draw(target, states);
+}
+
+void SceneNode::updateChildren(float dt)
+{
+    for (auto& child : m_children) child->update(dt);
+}
+
+sf::Transform SceneNode::getWorldTransform() const
+{
+    sf::Transform xform = sf::Transform::Identity;
+
+    // traverse the tree upwards
+    for (const SceneNode* node = this; node != nullptr; node = node->m_parent)
+        xform = node->getTransform() * xform;
+
+    return xform;
+}
--- src/scenenode.h
+++ src/scenenode.h
@@ -8,56 +8,22 @@ class SceneNode
   , private sf::NonCopyable
 {
 public:
-    void attachChild(std::unique_ptr<SceneNode> child)
-    {
-        child->m_parent = this;
-        m_children.push_back(std::move(child));
-    }
-
-    std::unique_ptr<SceneNode> detachChild(const SceneNode& node)
-    {
-        auto found = std::find_if(m_children.begin(), m_children.end(),
-                                  [&](std::unique_ptr<SceneNode>& p) -> bool {
-                                      return p.get() == &node;
-                                  });
-
-        assert(found != m_children.end());
-        std::unique_ptr<SceneNode> result = std::move(*found);
-        result->m_parent = nullptr;
-        m_children.erase(found);
-
-        return result;
-    }
-
-    void update(float dt)
-    {
-        updateCurrent(dt);
-        updateChildren(dt);
-    }
+    void attachChild(std::unique_ptr<SceneNode> child);
+    std::unique_ptr<SceneNode> detachChild(const SceneNode& node);
+    void update(float dt);
+    sf::Transform getWorldTransform() const;
 
 private:
     virtual void draw(sf::RenderTarget& target,
-                      sf::RenderStates states) const override final
-    {
-        states.transform *= getTransform();
-
-        drawCurrent(target, states);
-
-        // draw children
-        for (const auto& child : m_children)
-            child->draw(target, states);
-    }
+                      sf::RenderStates states) const override final;
 
     virtual void drawCurrent(sf::RenderTarget& target,
-                             sf::RenderStates states) const {}
+                             sf::RenderStates states) const
+    {}
 
     virtual void updateCurrent(float dt) {}
 
-    void updateChildren(float dt)
-    {
-        for (auto& child : m_children)
-            child->update(dt);
-    }
+    void updateChildren(float dt);
 
 private:
     std::vector<std::unique_ptr<SceneNode>> m_children;
